2a241d01cfcc789e5a37f2513bb791be3fac7893
FriskyXE <supanat.tinz@gmail.com>
2024-07-16 01:04:56 +0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,5 +1,9 @@
 Config = {}
 
+-- label = name boat to show in UI
+-- model = model boat for rental [https://gtahash.ru/car/?c=boats]
+-- price = price for rental
+-- image = image show preview in UI
 Config.Boats = {
     {label = 'Dinghy', model = 'dinghy', price = 100, image = 'https://media.discordapp.net/attachments/673503448427266059/1261988116949110834/dinghy.png?ex=6694f585&is=6693a405&hm=7470fa5f64ad82ea91679a28666c6c711e91261c7bcf8a27e161c7ac4d6da140&=&format=webp&quality=lossless&width=550&height=310'},
     {label = 'Jetmax', model = 'jetmax', price = 150, image = 'https://media.discordapp.net/attachments/673503448427266059/1261988257412157530/jetmax.png?ex=6694f5a7&is=6693a427&hm=38bfdfa63252b804f6ce29dac403168618d89fc2c8ebe34545ba782ff1c49cf3&=&format=webp&quality=lossless&width=550&height=310'}
