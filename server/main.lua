0031addc9383b3fc21e1a27ed23064aa85a95e91
FriskyXE <supanat.tinz@gmail.com>
2024-08-11 17:29:46 +0700

    Update notify

--- server/main.lua
+++ server/main.lua
@@ -1,35 +1,23 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
 RegisterNetEvent('blk-rentals:server:rentBoat', function(boat, coords)
-    local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
-    if Player.Functions.RemoveMoney('cash', boat.price, 'boat-rental') then
-        TriggerClientEvent('blk-rentals:client:spawnBoat', src, boat, coords)
+	local src = source
+	local Player = QBCore.Functions.GetPlayer(src)
+	if Player.Functions.RemoveMoney('cash', boat.price, 'boat-rental') then
+		TriggerClientEvent('blk-rentals:client:spawnBoat', src, boat, coords)
     else
-        TriggerClientEvent('ox_lib:notify', src, {
-            description = 'You do not have enough money!',
-            type = 'error',
-            position = 'center-right'
-        })
+		Config.Notify(src, 'You do not have enough money!', 'error', 'center-right')
     end
 end)
 
 RegisterNetEvent('blk-rentals:server:returnBoat', function(boat)
-    local src = source
+	local src = source
     local Player = QBCore.Functions.GetPlayer(src)
 
     if boat.price then
         Player.Functions.AddMoney('cash', boat.price, 'boat-rental-return') -- Refund based on price
-        TriggerClientEvent('ox_lib:notify', src, {
-            description = 'You have received your refund of $' .. boat.price .. ' back.',
-            type = 'success',
-            position = 'center-right'
-        })
+		Config.Notify(src, 'You have received your refund of $' .. boat.price .. ' back.', 'success', 'center-right')
     else
-        TriggerClientEvent('ox_lib:notify', src, {
-            description = 'Error: Boat price not found.',
-            type = 'error',
-            position = 'center-right'
-        })
+		Config.Notify(src, 'Error: Boat price not found.', 'error', 'center-right')
     end
 end)
